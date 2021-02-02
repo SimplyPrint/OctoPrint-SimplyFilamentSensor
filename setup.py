9665dd1173dd64da9fc060ca3e482165c4ef4fb0
Albert Møller Nielsen <albert@mollernielsen.dk>
2021-02-02 12:54:07 +0100

    Removed last faulthandler dep.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/SimplyPrint/OctoPrint-SimplyFilamentSensor"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["RPi.GPIO", "faulthandler"]
+plugin_requires = ["RPi.GPIO"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
